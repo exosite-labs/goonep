99e19f4d6feb29f3900d992077b7ae8bee56ca02
Daniel Letscher <dpletscher@outlook.com>
2015-08-04 17:21:57 -0500

    initial provision commit

--- provision.go
+++ provision.go
@@ -9,9 +9,18 @@ import (
     "io/ioutil"
     "regexp"
     "time"
+    "strconv"
 )
 
 var VendorToken = ""
+
+var PROVISION_BASE = "/provision"
+var PROVISION_ACTIVATE = PROVISION_BASE + "/activate"
+var PROVISION_DOWNLOAD = PROVISION_BASE + "/download"
+var PROVISION_MANAGE = PROVISION_BASE + "/manage"
+var PROVISION_MANAGE_MODEL = PROVISION_MANAGE + "/model/"
+var PROVISION_MANAGE_CONTENT = PROVISION_MANAGE + "/content/"
+var PROVISION_REGISTER = PROVISION_BASE + "/register"
 
 var Pool struct {
     Models  map[string]*ProvModel
@@ -22,14 +31,17 @@ type ProvContent struct {}
 type ProvGroup struct {}
 
 type ProvModel struct {
-    RawData         string
+    RawData             string
 
-    ActiveStatus    string
-    Rid             string
-    SN              string
+    ActiveStatus        string
+    Rid                 string
+    SN                  string
 
-    ExtraField      string
-    TimeStamp       int64
+    ExtraField          string
+    TimeStamp           int64
+
+    managebycik         bool
+    managebysharecode   bool
 }
 
 func (m *ProvModel) GetPath() string {
@@ -49,7 +61,8 @@ func (m *ProvModel) Find(modelName, id string) ProvModel {
         return ProvModel{}
     }
 
-    result, err := ProvCall( VendorToken, "GET", m.GetPath() + "/" + modelName + "/" + id )
+    var headers = http.Header{}
+    result, err := ProvCall( m.GetPath() + "/" + modelName + "/" + id, VendorToken, "", "GET", false, headers )
 
     if err != nil {
         log.Printf("Finding model(id: %s) met some error %v", id, err)
@@ -137,10 +150,9 @@ type ProvRestModel interface {
 
 }
 
-func ProvCall(vendorToken, method, path string) (interface{}, error) {
+func ProvCall(path, key, data, method string, managebycik bool, extra_headers http.Header) (interface{}, error) {
     client := &http.Client{}
 
-    // https://m2.exosite.com/provision/manage/model/flow_sensor/POC_FLOW_01
     var serverUrl = ""
     serverUrl = "https://" + ONEPHost + "/provision/"
     if InDev {
@@ -148,7 +160,7 @@ func ProvCall(vendorToken, method, path string) (interface{}, error) {
     }
 
     req, _ := http.NewRequest(method, serverUrl + path, nil)            
-    req.Header.Add("X-Exosite-Token", vendorToken)
+    req.Header.Add("X-Exosite-Token", key)
 
     resp, err := client.Do(req)
 
@@ -164,4 +176,192 @@ func ProvCall(vendorToken, method, path string) (interface{}, error) {
     }
 
     return body, nil
+}
+
+func content_create(provModel ProvModel, key, model, contentid, meta string, protect bool) (interface{}, error) {
+    var data = "id=" + contentid + "&meta=" + meta
+    if protect != false {
+        data = data + "&protected=true"
+    }
+    var path = PROVISION_MANAGE_CONTENT + model + "/"
+    var headers = http.Header{}
+    return ProvCall(path, key, data, "POST", provModel.managebycik, headers)
+}
+
+func content_download(provModel ProvModel, cik, vendor, model, contentid string) (interface{}, error) {
+    var data = "vendor=" + vendor + "&model=" + model + "&id=" + contentid
+    var headers = http.Header{}
+    headers.Add("Accept",  "*")
+    return ProvCall(PROVISION_DOWNLOAD, cik, data, "GET", provModel.managebycik, headers)
+}
+
+func content_info(provModel ProvModel, key, model, contentid, vendor string) (interface{}, error) {
+    var headers = http.Header{}
+    if vendor == "" {
+        var path = PROVISION_MANAGE_CONTENT + model + "/" + contentid
+        return ProvCall(path, key, "", "GET", provModel.managebycik, headers)
+    } else {
+        var data = "vendor=" + vendor + "&model=" + model + "&info=true"
+        return ProvCall(PROVISION_DOWNLOAD, key, data, "GET", provModel.managebycik, headers)
+    }
+}
+
+func content_list(provModel ProvModel, key, model string) (interface{}, error) {
+    var path = PROVISION_MANAGE_CONTENT + model + "/"
+    var headers = http.Header{}
+    return ProvCall(path, key, "", "GET", provModel.managebycik, headers)
+}
+
+func content_remove(provModel ProvModel, key, model, contentid string) (interface{}, error) {
+    var headers = http.Header{}
+    var path = PROVISION_MANAGE_CONTENT + model + "/" + contentid
+    return ProvCall(path, key, "", "DELETE", provModel.managebycik, headers)
+}
+
+func content_upload(provModel ProvModel, key, model, contentid, data, mimetype string) (interface{}, error) {
+    var headers = http.Header{}
+    headers.Add("Content-Type", mimetype)
+    var path = PROVISION_MANAGE_CONTENT + model + "/" + contentid
+    return ProvCall(path, key, data, "POST", provModel.managebycik, headers)
+}
+
+func model_create(provModel ProvModel, key, model, sharecode string, aliases, comments, historical bool) (interface{}, error) {
+    var headers = http.Header{}
+    var data = "model=" + model
+    if provModel.managebysharecode {
+        data = data + "&code=" + sharecode
+    } else {
+        data = data + "&rid=" + sharecode
+    }
+    if aliases == false {
+        data = data + "&options[]=noaliases"
+    }
+    if comments == false {
+        data = data + "&options[]=nocomments"
+    }
+    if historical == false {
+        data = data + "&options[]=nohistorical"
+    }
+    return ProvCall(PROVISION_MANAGE_MODEL, key, data, "POST", provModel.managebycik, headers)
+}
+
+func model_info(provModel ProvModel, key, model string) (interface{}, error) {
+    var headers = http.Header{}
+    return ProvCall(PROVISION_MANAGE_MODEL + model, key, "", "GET", provModel.managebycik, headers)
+}
+
+func model_list(provModel ProvModel, key string) (interface{}, error) {
+    var headers = http.Header{}
+    return ProvCall(PROVISION_MANAGE_MODEL, key, "", "GET", provModel.managebycik, headers)
+}
+
+func model_remove(provModel ProvModel, key, model string) (interface{}, error) {
+    var headers = http.Header{}
+    var data = "delete=true&model=" + model + "&confirm=true"
+    var path = PROVISION_MANAGE_MODEL + model
+    return ProvCall(path, key, data, "DELETE", provModel.managebycik, headers)
+}
+
+func model_update(provModel ProvModel, key, model, clonerid string, aliases, comments, historical bool) (interface{}, error) {
+    var headers = http.Header{}
+    var data = "rid=" + clonerid
+    var path = PROVISION_MANAGE_MODEL + model
+    return ProvCall(path, key, data, "PUT", provModel.managebycik, headers)
+}
+
+func serialnumber_activate(provModel ProvModel, model, serialnumber, vendor string) (interface{}, error) {
+    var headers = http.Header{}
+    var data = "vendor=" + vendor + "&model=" + model + "&sn=" + serialnumber
+    return ProvCall(PROVISION_ACTIVATE, "", data, "POST", provModel.managebycik, headers)
+}
+
+func serialnumber_add(provModel ProvModel, key, model, sn string) (interface{}, error) {
+    var headers = http.Header{}
+    var data = "add=true&sn=" + sn
+    var path = PROVISION_MANAGE_MODEL + model + "/"
+    return ProvCall(path, key, data, "POST", provModel.managebycik, headers)
+}
+
+func serialnumber_add_batch(provModel ProvModel, key, model string, sns []string) (interface{}, error) {
+    var headers = http.Header{}
+    var data = "add=true"
+    for i := range sns {
+        data = data + "&sn[]=" + sns[i]
+    }
+    var path = PROVISION_MANAGE_MODEL + model + "/"
+    return ProvCall(path, key, data, "POST", provModel.managebycik, headers)
+}
+
+func serialnumber_disable(provModel ProvModel, key, model, serialnumber string) (interface{}, error) {
+    var headers = http.Header{}
+    var data = "disable=true"
+    var path = PROVISION_MANAGE_MODEL + model + "/" + serialnumber
+    return ProvCall(path, key, data, "POST", provModel.managebycik, headers)
+}
+
+func serialnumber_enable(provModel ProvModel, key, model, serialnumber, owner string) (interface{}, error) {
+    var headers = http.Header{}
+    var data = "enable=true&owner=" + owner
+    var path = PROVISION_MANAGE_MODEL + model + "/" + serialnumber
+    return ProvCall(path, key, data, "POST", provModel.managebycik, headers)
+}
+
+func serialnumber_info(provModel ProvModel, key, model, serialnumber string) (interface{}, error) {
+    var headers = http.Header{}
+    var path = PROVISION_MANAGE_MODEL + model + "/" + serialnumber
+    return ProvCall(path, key, "", "GET", provModel.managebycik, headers)
+}
+
+func serialnumber_list(provModel ProvModel, key, model string, offset, limit int) (interface{}, error) {
+    var headers = http.Header{}
+    var data = "offset=" + strconv.Itoa(offset) + "&limit=" + strconv.Itoa(limit)
+    var path = PROVISION_MANAGE_MODEL + model + "/"
+    return ProvCall(path, key, data, "GET", provModel.managebycik, headers)
+}
+
+func serialnumber_reenable(provModel ProvModel, key, model, serialnumber string) (interface{}, error) {
+    var headers = http.Header{}
+    var data = "enable=true"
+    var path = PROVISION_MANAGE_MODEL + model + "/" + serialnumber
+    return ProvCall(path, key, data, "POST", provModel.managebycik, headers)
+}
+
+func serialnumber_remap(provModel ProvModel, key, model, serialnumber, oldsn string) (interface{}, error) {
+    var headers = http.Header{}
+    var data = "enable=true&oldsn=" + oldsn
+    var path = PROVISION_MANAGE_MODEL + model + "/" + serialnumber
+    return ProvCall(path, key, data, "POST", provModel.managebycik, headers)
+}
+
+func serialnumber_remove(provModel ProvModel, key, model, serialnumber string) (interface{}, error) {
+    var headers = http.Header{}
+    var path = PROVISION_MANAGE_MODEL + model + "/" + serialnumber
+    return ProvCall(path, key, "", "DELETE", provModel.managebycik, headers)
+}
+
+func serialnumber_remove_batch(provModel ProvModel, key, model string, sns []string) (interface{}, error) {
+    var headers = http.Header{}
+    var data = "remove=true"
+    for i := range sns {
+        data = data + "&sn[]=" + sns[i]
+    }
+    var path = PROVISION_MANAGE_MODEL + model + "/"
+    return ProvCall(path, key, data, "POST", provModel.managebycik, headers)
+}
+
+func vendor_register(provModel ProvModel, key, vendor string) (interface{}, error) {
+    var headers = http.Header{}
+    var data = "vendor=" + vendor
+    return ProvCall(PROVISION_REGISTER, key, data, "POST", provModel.managebycik, headers)
+}
+
+func vendor_show(key string) (interface{}, error) {
+    var headers = http.Header{}
+    return ProvCall(PROVISION_REGISTER, key, "", "GET", false, headers)
+}
+
+func vendor_unregister(key, vendor string) (interface{}, error) {
+    var headers = http.Header{}
+    var data = "delete=true&vendor=" + vendor
+    return ProvCall(PROVISION_REGISTER, key, data, "POST", false, headers)
 }
